fa05eb1f2ad2333d476b1ff1621cb4705f167186
Aniket Hirpara <gdg4dev@gmail.com>
2024-11-11 18:05:48 -0500

    ip fix#1

--- app.js
+++ app.js
@@ -133,14 +133,41 @@ app.get('/icon/:trackingId', async (req, res) => {
   if (!trackingId) return;
 
   let session = null;
+  const FALLBACK_IP_ADDRESS = '0.0.0.0';
 
+  function realIP(request, cfProxy = false) {
+    const headers = request.headers;
+  
+
+    if (cfProxy && headers.has('cf-connecting-ip')) {
+      return headers.get('cf-connecting-ip');
+    }
+  
+    if (headers.has('x-real-ip')) {
+      return headers.get('x-real-ip');
+    }
+  
+    if (headers.has('x-forwarded-for')) {
+      return headers.get('x-forwarded-for');
+    }
+  
+    if (headers.has('x-vercel-forwarded-for')) {
+      return headers.get('x-vercel-forwarded-for');
+    }
+  
+    if (headers.has('x-vercel-proxied-for')) {
+      return headers.get('x-vercel-proxied-for');
+    }
+  
+    return request.ip ?? FALLBACK_IP_ADDRESS;
+  }
   try {
     const conn = await connectToDatabase();
     session = await conn.startSession();
 
     await session.withTransaction(async () => {
       const userAgent = req.headers['user-agent'] || 'unknown';
-      const ip = req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.ip || 'unknown';
+      const ip = await realIP(req, true);
       const timestamp = new Date();
 
       const updateResult = await Email.findOneAndUpdate(
